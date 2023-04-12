5c1625cc38bd5955ec8fd8bcfbe240e762472fdb
SU YEON.L <129818934+LEESUSUSUSU@users.noreply.github.com>
2023-04-12 18:36:45 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 212 ms, Memory: 17756 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int A = 0;
+		int B = 0;
+	
+	
+	    Scanner sc = new Scanner(System.in);
+        A = sc.nextInt();
+        B = sc.nextInt();
+        
+        if(A>B) {
+        	System.out.println(">");
+        	
+        }else if(A<B) {
+           System.out.println("<");   
+	}else if (A == B) {
+		System.out.println("==");
+	}
+ }
+}
