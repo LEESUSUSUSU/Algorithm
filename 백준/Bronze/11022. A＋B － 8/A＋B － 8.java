6913c75582e44044d98ee995dfc739d9cba4f092
SU YEON.L <129818934+LEESUSUSUSU@users.noreply.github.com>
2023-04-19 12:53:27 +0900

    [Bronze V] Title: A+B - 8, Time: 268 ms, Memory: 18996 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,25 @@
+
+
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int t = sc.nextInt();
+
+		for (int i = 1; i <= t; i++) {
+
+			int a, b;
+			a = sc.nextInt();
+			b = sc.nextInt();
+			//Case #1: 1 + 1 = 2
+			System.out.println("Case #" + i +": "+ a + " + " + b + " = " + (a + b));
+
+		}
+
+	}
+}
