71432fe160e81b03c2d89c3af05bafebd6ff9b40
SU YEON.L <129818934+LEESUSUSUSU@users.noreply.github.com>
2023-04-19 19:26:58 +0900

    [Bronze V] Title: A+B - 4, Time: 244 ms, Memory: 17828 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,21 @@
+
+
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		while (sc.hasNext()) {  // 나가는 방법을 몰라서 알아봄 (정수 외 값이 나오면 false 나온다.)
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+
+			System.out.println(a + b);
+
+		}
+
+	}
+}
