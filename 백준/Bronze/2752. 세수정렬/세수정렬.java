7fb57349f3c089db7cea9054a4a41e2890e6e1ad
SU YEON.L <129818934+LEESUSUSUSU@users.noreply.github.com>
2023-05-28 20:57:15 +0900

    [Bronze IV] Title: 세수정렬, Time: 228 ms, Memory: 18392 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.java
@@ -0,0 +1,29 @@
+
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args)throws Exception {
+		Scanner sc = new Scanner(System.in);
+		
+		int [] arr =new int[3];
+		
+		for(int i =0; i<arr.length; i++) {
+			
+			arr [i]= sc.nextInt();
+			
+		}
+		
+		
+		Arrays.sort(arr);
+	
+		for(int i =0; i<arr.length; i++) {
+			System.out.print(arr[i]+" ");
+		}
+		
+	}
+
+}
