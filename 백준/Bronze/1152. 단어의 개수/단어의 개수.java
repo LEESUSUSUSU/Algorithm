0d5798630a684680e61321eb4fc1aeff44c8e8a5
SU YEON.L <129818934+LEESUSUSUSU@users.noreply.github.com>
2023-07-10 10:43:49 +0900

    [Bronze II] Title: 단어의 개수, Time: 312 ms, Memory: 26532 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,24 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        ArrayList<String> d = new ArrayList<>();
+
+        while (st.hasMoreTokens()) {  // 반환할게 있으면 true 없으면 False
+            String token = st.nextToken();
+
+            d.add(token);
+        }
+
+        System.out.println(d.size());
+    }
+}
